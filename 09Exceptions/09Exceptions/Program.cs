be6e86333c3f79a52591e6af8d42ecfc0366136f
georstef <georstef@gmail.com>
2013-12-08 16:31:43 +0200

    09

---
+++ 09Exceptions/09Exceptions/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _09Exceptions
+{
+  public class BankException : System.Exception 
+  { 
+    public BankException(string message) : base(message) 
+    {
+      WriteStuff("Parent: " + message);
+    }
+
+    public BankException()
+    {
+      //must have this parameter-less method here in the parent
+      //so that the child constructor doesn't have to use [:base(message)]
+    }
+    
+    public void WriteStuff(string message)
+    {
+      Console.WriteLine(message);
+    }
+  }
+
+  public class MyException: BankException
+  {
+    public MyException(string message)
+    {
+      //in C# the parents method is always called before the childs method
+      //base.Exception(message); <= does not work
+
+      base.WriteStuff("Child: " + message);
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        if ("john" != "") //inName.Length == 0) 
+        {
+          throw new MyException("Invalid Name");
+        }
+      }
+      catch (MyException e)
+      {
+        //Console.WriteLine(e.Message);
+        //throw;
+      }
+
+      Console.ReadKey();
+    }
+  }
+}
