d4bc3e174acf0b783e3f17c7865e21bd9aa68117
georstef <georstef@gmail.com>
2013-12-14 12:08:33 +0200

    10

---
+++ 10LibraryDLL/10LibraryDLL1/MyDLLClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _10LibraryDLL
+{
+  public class MyDLLClass
+  {
+    public static void WriteFromDLL(string s)
+    {
+      Console.WriteLine("From dll: "+s);
+    }
+  }
+}
