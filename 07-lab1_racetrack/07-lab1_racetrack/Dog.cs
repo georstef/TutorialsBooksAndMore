807d7c8ad88e0aa8af633853f52a726e9278fdaf
georstef <georstef@gmail.com>
2013-12-23 21:27:21 +0200

    07-lab1

---
+++ 07-lab1_racetrack/07-lab1_racetrack/Dog.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace _07_lab1_racetrack
+{
+  class Dog
+  {
+    public string lane;
+    public Image imgDog;
+    public int currentPosition;//the left part
+    int width;
+
+    public Dog(string laneIn, Image imgDogIn, int widthIn, int initialPosition)
+    {
+      lane = laneIn;
+      imgDog = imgDogIn;
+      width = widthIn;
+      setPosition(initialPosition);
+    }
+
+    public void Move(int steps)
+    {
+      currentPosition += steps;
+      changePosition();
+    }
+
+    public bool hasFinished(int finishLine)
+    {
+      return (currentPosition+width >= finishLine);
+    }
+
+    public void setPosition(int position)
+    {
+      currentPosition = position;
+      changePosition();
+    }
+
+    private void changePosition()
+    {
+      Canvas.SetLeft(imgDog, currentPosition);
+    }
+
+  }
+}
