7dd47ec592dcf1c66c1e240f2f405f05b0f9fbe1
georstef <georstef@gmail.com>
2014-02-16 21:21:01 +0200

    17
    exact copy from the book examples

---
+++ 17-chapter14-linq-wpf/17-chapter14-linq-wpf/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace _17_chapter14_linq_wpf
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+  }
+}
