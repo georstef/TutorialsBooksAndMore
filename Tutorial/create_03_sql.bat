0be96ce025b69918b992857188e5e6f415e4583a
georstef <georstef@gmail.com>
2014-03-17 18:29:40 +0200

    Tutorial 1 & 2

---
+++ Tutorial/create_03_sql.bat
@@ -0,0 +1,13 @@
+manage.py sql polls
+manage.py sqlcustom polls 
+manage.py sqlindexes polls
+
+@rem check for errors
+manage.py validate 
+
+@rem drop table statements
+manage.py sqlclear polls 
+
+@rem combination of sql, sqlcustom, sqlindexes
+@rem manage.py sqlall polls 
+pause
