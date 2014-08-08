899ac47f2695c66e7879943eebdbfe6f6236533f
georstef <georstef@gmail.com>
2014-08-08 22:07:06 +0300

    stackoverflow (admin actions)

--- stackoverflow/aggregates/admin.py
+++ stackoverflow/aggregates/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from aggregates.models import Match, Team
 
-
+def reverse_goals(modeladmin, request, queryset):
+    for m in queryset:
+        m.goals_team_a, m.goals_team_b = m.goals_team_b, m.goals_team_a
+        m.save()
+reverse_goals.short_description = "Reverse the goals of the match"
+    
 class MatchAdmin(admin.ModelAdmin):
     list_display = ('team_vs_team_score',)
+    actions = [reverse_goals,]
 
 
 # Register your models here.
