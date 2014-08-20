2efd1ba16e1dd3255b989fa30d7449d1a6be01c5
georstef <georstef@gmail.com>
2014-08-20 21:02:08 +0300

    portfolio (comments for templates folder)

--- portfolio/work/views.py
+++ portfolio/work/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def album_list(request):
-    return render(request, 'album_list.html')
+    return render(request, 'album_list.html')  # it searches inside work/templates
+    # return render(request, 'work/album_list.html')  # with 'work/' it searches inside work/templates/work
