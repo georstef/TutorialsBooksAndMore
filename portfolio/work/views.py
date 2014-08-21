2952f384b4e02cc18b109477370e589f4be1ce29
georstef <georstef@gmail.com>
2014-08-21 20:55:21 +0300

    portfolio (base created and static changed)

--- portfolio/work/views.py
+++ portfolio/work/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from work.models import Album, Photo
 
 def album_list(request):
-    return render(request, 'album_list.html')  # it searches inside work/templates
+    albums = Album.objects.filter(published_date__isnull=False).order_by('-published_date')
+    return render(request, 'album_list.html', {'albums': albums})  # it searches inside work/templates
     # return render(request, 'work/album_list.html')  # with 'work/' it searches inside work/templates/work
