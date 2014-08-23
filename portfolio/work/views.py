498506417ee3550f52a4e6c1b05bcb1643f48150
georstef <georstef@gmail.com>
2014-08-23 20:55:03 +0300

    portfolio (static changes)

--- portfolio/work/views.py
+++ portfolio/work/views.py
@@ -3,5 +3,5 @@ from work.models import Album, Photo
 
 def album_list(request):
     albums = Album.objects.filter(published_date__isnull=False).order_by('-published_date')
-    return render(request, 'album_list.html', {'albums': albums})  # it searches inside work/templates
+    return render(request, 'work/album_list.html', {'albums': albums})  # it searches inside work/templates
     # return render(request, 'work/album_list.html')  # with 'work/' it searches inside work/templates/work
