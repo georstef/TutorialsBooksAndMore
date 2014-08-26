2506fd961c90eb091c4c4f742b0cbe976ba0e8bb
georstef <georstef@gmail.com>
2014-08-26 20:25:02 +0300

    portfolio (contact final)

--- portfolio/contact/views.py
+++ portfolio/contact/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from contact.models import Contact
+
 
 def contact_detail(request):
-    # album = get_object_or_404(Album, published_date__isnull=False, pk=id)
-    return render(request, 'contact/contact_detail.html', {})  # it searches inside contact/templates/contact
-    # return render(request, 'work/album_detail.html', {'album': album})  # it searches inside contact/templates
+    contacts = Contact.objects.all()
+    return render(request, 'contact/contact.html', {'contacts': contacts})
