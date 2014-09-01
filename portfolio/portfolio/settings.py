c2c5b50a0786e3f504148625b423af41e2700db1
georstef <georstef@gmail.com>
2014-09-01 22:23:08 +0300

    portfolio (image align)

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -101,5 +101,5 @@ MEDIA_URL = '/media_folder/'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  # or 587
 EMAIL_HOST_USER = 'nwohmb@gmail.com'
-EMAIL_HOST_PASSWORD = '****'
+EMAIL_HOST_PASSWORD = '***'
 EMAIL_USE_TLS = True
