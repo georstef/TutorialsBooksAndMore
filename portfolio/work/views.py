f905ae04c3532b46730f273414cb07ffed3b8c1c
georstef <georstef@gmail.com>
2014-09-11 21:45:46 +0300

    portfolio (pagination)

--- portfolio/work/views.py
+++ portfolio/work/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from work.models import Album, Photo
 
+
 def album_list(request):
-    albums = Album.objects.filter(published_date__isnull=False).order_by('-published_date')
+    albums_list = Album.objects.filter(published_date__isnull=False).order_by('-published_date')
+    paginator = Paginator(albums_list, 2)
+
+    page = request.GET.get('page')
+    try:
+        albums = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer, deliver first page.
+        albums = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range (e.g. 9999), deliver last page of results.
+        albums = paginator.page(paginator.num_pages)
     return render(request, 'work/album_list.html', {'albums': albums})  # it searches inside work/templates/work
     # return render(request, 'album_list.html')  # without 'work/' it searches inside work/templates
 
