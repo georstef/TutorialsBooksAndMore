b488d3c83fef14a93c1a3d96397245856ac1ec57
georstef <georstef@gmail.com>
2014-09-13 11:43:12 +0300

    portfolio (comments deleted)

--- portfolio/work/urls.py
+++ portfolio/work/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import patterns, url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from work import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.album_list, name='albumlist'),
                        url(r'^album/(?P<id>[0-9]+)/$', views.album_detail, name='albumdetail'),
                        )
-
-# urlpatterns += staticfiles_urlpatterns()
