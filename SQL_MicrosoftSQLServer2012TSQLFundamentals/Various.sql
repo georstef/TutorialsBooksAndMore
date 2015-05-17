9a4018ab93ef37634b66c0fe4296f5666e5f2ef1
George S. <georstef@gmail.com>
2015-05-17 20:11:40 +0300

    Update Various.sql

--- SQL_MicrosoftSQLServer2012TSQLFundamentals/Various.sql
+++ SQL_MicrosoftSQLServer2012TSQLFundamentals/Various.sql
@@ -3,3 +3,9 @@ select DATEADD(month, DATEDIFF(month, '19990101', current_timestamp), '19990101'
 
 -- last day of the month
 select DATEADD(month, DATEDIFF(month, '19991231', current_timestamp), '19991231');
+
+-- get last identity value
+SELECT
+  SCOPE_IDENTITY() AS [SCOPE_IDENTITY],        -- current session, current scope
+  @@identity AS [@@identity],                  -- current session, regardless scope
+  IDENT_CURRENT('dbo.table_name') AS [IDENT_CURRENT];  -- regardless session, regardless scope
