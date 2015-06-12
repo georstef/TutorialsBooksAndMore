fe87764e0de2ef569f18596d6283dbffd07d289c
George S. <georstef@gmail.com>
2015-06-12 17:09:26 +0300

    Update Various-Pivot.sql

--- SQL_MicrosoftSQLServer2012TSQLFundamentals/Various-Pivot.sql
+++ SQL_MicrosoftSQLServer2012TSQLFundamentals/Various-Pivot.sql
@@ -1,3 +1,6 @@
+-- -------------------------------------------------
+-- 1. Dynamic SQL Pivot with STUFF -----------------
+-- -------------------------------------------------
 IF OBJECT_ID('tempdb.dbo.#CustomerProducts') IS NOT NULL
 DROP TABLE dbo.#CustomerProducts;
 GO
@@ -34,3 +37,68 @@ set @query = 'select * from #CustomerProducts
  
  
 execute(@query) 
+
+-- -------------------------------------------------
+-- 2. Dynamic SQL Pivot with Cursor ----------------
+-- -------------------------------------------------
+SET NOCOUNT ON
+
+DECLARE 
+  @orderyear int,
+  @pivotfields nvarchar(200),
+  @sql AS nVARCHAR(200);
+  
+DECLARE C CURSOR FOR
+  SELECT distinct YEAR(orderdate) AS orderyear FROM Sales.Orders;
+  
+OPEN C;
+
+FETCH NEXT FROM C INTO @orderyear;
+SET @pivotfields = ''
+
+while @@fetch_status = 0 BEGIN
+  set @pivotfields = @pivotfields + QUOTENAME(@orderyear)+N',';
+  
+  FETCH NEXT FROM C INTO @orderyear;
+END
+
+DEALLOCATE C;
+
+set @pivotfields = left(@pivotfields, len(@pivotfields)-1); -- last comma
+
+set @sql = 'SELECT *
+FROM 
+  (SELECT shipperid, YEAR(orderdate) AS orderyear, freight FROM Sales.Orders) AS D
+PIVOT
+  (
+   SUM(freight) FOR orderyear IN('+@pivotfields+')) AS P
+   order by shipperid'
+
+EXEC sp_executesql
+  @stmt = @sql;
+
+-- -------------------------------------------------
+-- 3. Dynamic SQL Pivot with STUFF -----------------
+-- the same data as the 2nd example ----------------
+-- -------------------------------------------------
+SET NOCOUNT ON
+
+DECLARE 
+  @orderyear int,
+  @pivotfields nvarchar(200),
+  @sql AS nVARCHAR(200);
+  
+set @pivotfields = STUFF((SELECT distinct ',' + quotename(cast(YEAR(orderdate) as varchar))
+                        FROM Sales.Orders
+                        FOR XML PATH('') 
+                       ), 1, 1, '') 
+set @sql = 'SELECT *
+FROM 
+  (SELECT shipperid, YEAR(orderdate) AS orderyear, freight FROM Sales.Orders) AS D
+PIVOT
+  (
+   SUM(freight) FOR orderyear IN('+@pivotfields+')) AS P
+   order by shipperid'
+
+EXEC sp_executesql
+  @stmt = @sql;
