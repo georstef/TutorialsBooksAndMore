a0be2880fed54af5c317444e8f9b7be82c2b11df
George S. <georstef@gmail.com>
2015-06-12 17:00:08 +0300

    Update Various-Pivot.sql

--- SQL_MicrosoftSQLServer2012TSQLFundamentals/Various-Pivot.sql
+++ SQL_MicrosoftSQLServer2012TSQLFundamentals/Various-Pivot.sql
@@ -1,30 +1,36 @@
-CREATE TABLE CustomerProducts
-([CustomerName] varchar(20), [ProductName] varchar(20), [Ammount] int)
+IF OBJECT_ID('tempdb.dbo.#CustomerProducts') IS NOT NULL
+DROP TABLE dbo.#CustomerProducts;
+GO
 
-INSERT INTO CustomerProducts
-(CustomerName, ProductName, Ammount)
-VALUES
-('Customer1', 'Product1', 10),
-('Customer1', 'Product1', 100),
-('Customer2', 'Product2', 20),
-('Customer2', 'Product2', 200),
-('Customer1', 'Product2', 30),
-('Customer1', 'Product2', 300),
-('Customer2', 'Product1', 40),
-('Customer2', 'Product1', 400);
-
--------------------------------------------------------------------------
-
-DECLARE @products AS varchar(MAX)
-declare @query  AS NVARCHAR(MAX)
-set @products = STUFF((SELECT distinct ',' + ProductName
-                       FROM CustomerProducts
-                       FOR XML PATH('')
-                      ), 1, 1, '')
-
-set @query = 'select * from CustomerProducts
-              pivot (sum(Ammount)                  -- data to pivot
-                for ProductName in ('+@products+') -- column name
-               ) as C'
-
-execute(@query)
+CREATE TABLE #CustomerProducts 
+([CustomerName] varchar(20), [ProductName] varchar(20), [Amount] int) 
+ 
+INSERT INTO #CustomerProducts 
+ (CustomerName, ProductName, Amount) 
+VALUES 
+ ('Customer1', 'Product1', 10), 
+ ('Customer1', 'Product1', 100), 
+ ('Customer2', 'Product2', 20), 
+ ('Customer2', 'Product2', 200), 
+ ('Customer1', 'Product2', 30), 
+ ('Customer1', 'Product2', 300), 
+ ('Customer2', 'Product1', 40), 
+ ('Customer2', 'Product1', 400); 
+ 
+-- --------------------------------- 
+ 
+DECLARE @products AS varchar(MAX) 
+declare @query  AS NVARCHAR(MAX) 
+set @products = STUFF((SELECT distinct ',' + ProductName 
+                        FROM #CustomerProducts 
+                        FOR XML PATH('') 
+                       ), 1, 1, '') 
+ 
+ 
+set @query = 'select * from #CustomerProducts 
+               pivot (sum(Amount)                  -- data to pivot 
+                 for ProductName in ('+@products+') -- column name 
+                ) as C' 
+ 
+ 
+execute(@query) 
