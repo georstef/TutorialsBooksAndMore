50c169e2e6822ab165beefd70e69f200f75a7466
George S <georstef@gmail.com>
2016-10-19 19:47:52 +0300

    dotNet tries

---
+++ dotNET/simpleMVVM/WpfApplication1/vmodel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApplication1
+{
+    public class Presenter : ObservableObject
+    {
+        private readonly TextConverter _textConverter = new TextConverter(s => s.ToUpper());
+        private string _someText;
+        private readonly ObservableCollection<string> _history = new ObservableCollection<string>();
+
+        public string SomeText
+        {
+            get { return _someText; }
+            set
+            {
+                _someText = value;
+                RaisePropertyChangedEvent("SomeText");
+            }
+        }
+
+        public IEnumerable<string> History
+        {
+            get { return _history; }
+        }
+
+        public ICommand ConvertTextCommand
+        {
+            get { return new DelegateCommand(ConvertText); }
+        }
+
+        private void ConvertText()
+        {
+            if (string.IsNullOrWhiteSpace(SomeText)) return;
+            AddToHistory(_textConverter.ConvertText(SomeText));
+            SomeText = string.Empty;
+        }
+
+        private void AddToHistory(string item)
+        {
+            if (!_history.Contains(item))
+                _history.Add(item);
+        }
+    }
+
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action _action;
+
+        public DelegateCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        #pragma warning disable 67
+        public event EventHandler CanExecuteChanged;
+        #pragma warning restore 67
+    }
+}
--- dotNET_Full_Stack/GigHub/GigHub/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace GigHub.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
