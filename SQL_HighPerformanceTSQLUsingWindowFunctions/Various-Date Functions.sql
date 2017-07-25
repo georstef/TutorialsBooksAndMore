e2c92f21bfb21d54766bab9c8aa08c6a10d7b07d
George S <georstef@gmail.com>
2017-07-25 16:54:28 +0300

    Create Various-Date Functions.sql

---
+++ SQL_HighPerformanceTSQLUsingWindowFunctions/Various-Date Functions.sql
@@ -0,0 +1,20 @@
+-- ----------------------------------
+-- function to DATEADD only weekdays
+-- ----------------------------------
+CREATE FUNCTION [dbo].[DateAddWeekdays] 
+(
+   @FromDate datetime,
+   @DaysToAdd int
+)
+RETURNS datetime
+AS
+BEGIN
+   DECLARE @Result datetime
+
+   SET @Result = DATEADD(day, (@DaysToAdd % 5) + CASE ((@@DATEFIRST + DATEPART(weekday, @FromDate) + (@DaysToAdd % 5)) % 7)
+                                                 WHEN 0 THEN 2
+                                                 WHEN 1 THEN 1
+                                                 ELSE 0 END, DATEADD(week, (@DaysToAdd / 5), @FromDate))
+
+   RETURN @Result
+END
